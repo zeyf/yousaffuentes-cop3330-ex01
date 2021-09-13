cd06599df2809ef38de2f5bbee09878dabf868d7
Zain <zainy0012@gmail.com>
2021-09-13 19:36:07 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 zain yousaffuentes
+ */
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+      	String name = scanner.nextLine();
+      
+      	System.out.println("Hello, " + name + ", nice to meet you!");
+      
+    };
+};
